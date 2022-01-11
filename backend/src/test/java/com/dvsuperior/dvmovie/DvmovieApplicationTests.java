52c0f20dba08b9c161b0fa4118a5a91ab58098ef
Marcos M Santos <marcos1970moreirasantos@gmail.com>
2022-01-10 23:57:39 -0300

    Project created

---
+++ backend/src/test/java/com/dvsuperior/dvmovie/DvmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dvmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
